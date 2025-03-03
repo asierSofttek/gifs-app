aee72b17afca03850ba4536dda8f5996ffc9fbcb
Asier Zabala <asier.zabala@softtek.com>
2025-03-03 11:51:00 +0100

    upload gifs-app project

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '@environments/environment';
+import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../gif.mapper';
+import { map } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class GifService {
+
+  private http = inject(HttpClient)
+
+  trendingGifs = signal<Gif[]>([])
+  trendingGifsLoading = signal(true)
+
+  constructor() {
+    this.loadTrendingGifs();
+    console.log('Servicio creado');
+
+  }
+
+  loadTrendingGifs() {
+    this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20
+      }
+    }).subscribe((resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGifs.set(gifs);
+      this.trendingGifsLoading.set(false);
+      console.log({ gifs });
+    })
+  }
+
+  searchGifs(query: string) {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query,
+      }
+    }).pipe(
+      map(({ data }) => data),
+      map(items => GifMapper.mapGiphyItemsToGifArray(items)),
+
+    );
+
+
+    // .subscribe((resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+
+    //   console.log({ search: gifs });
+    // })
+
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+
+  giphyApiKey: 'SEVBYFm4EJF61RPGDH5vEoDGVJtbCtDr',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
