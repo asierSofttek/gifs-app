299ac979120abf277ab0730e1f8bf940b9d4f10f
Asier Zabala <asier.zabala@softtek.com>
2025-03-03 17:42:47 +0100

    feature: infinity scroll

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,30 +1,42 @@
-import { ChangeDetectionStrategy, Component, signal, inject, computed } from '@angular/core';
-import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { Component, inject, viewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
-
-// const imageUrls: string[] = [
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-// ];
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
+
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    const isABottom = scrollTop + clientHeight + 300 >= scrollHeight;
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
+    if (isABottom) {
+      this.gifService.loadTrendingGifs();
+    }
+
+
+  }
 
 }
